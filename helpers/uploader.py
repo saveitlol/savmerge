b7aa0eb9138c69dc31f0eb28cdc607aa97cdfd7a
Ahmad zaky <53550696+saveitlol@users.noreply.github.com>
2021-06-28 22:29:24 +0700

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -30,9 +30,9 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                        [InlineKeyboardButton("Developer - @saveitlol", url="https://t.me/saveitlol")],
+                        [InlineKeyboardButton("Mirror Group", url="https://t.me/svitdrive"),
+                         InlineKeyboardButton("Bots Channel", url="https://t.me/svbotif")]
                     ]
                 )
             )
@@ -51,9 +51,9 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [
-                        [InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005")],
-                        [InlineKeyboardButton("Support Group", url="https://t.me/linux_repo"),
-                         InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]
+                        [InlineKeyboardButton("Developer - @saveitlol", url="https://t.me/saveitlol")],
+                        [InlineKeyboardButton("Mirror Group", url="https://t.me/svitdrive"),
+                         InlineKeyboardButton("Bots Channel", url="https://t.me/svbotif")]
                     ]
                 )
             )
